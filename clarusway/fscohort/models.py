f16b74bdca9e183a8efc26e81c0d9814c19c5071
meltem <meltemkaraagac19@gmail.com>
2020-12-24 19:06:37 -0500

    add

--- clarusway/fscohort/models.py
+++ clarusway/fscohort/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+# from django.db.models import aggregates
 
 # Create your models here.
 
 
 class Student(models.Model):
-    first_name = models.CharField(max_length=50)
+    sir_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     number = models.IntegerField()
+    
+    def __str__(self):
+        return str(self.number)
+
+    
--- clarusway/fscohort/views.py
+++ clarusway/fscohort/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 
 def home_view(request):
-    return HttpResponse("Hi, this is fscohort Home page.")
+    return HttpResponse("<h1>Hi, this is fscohort Home page.</h1>")
 
 
 def about_view(request):
