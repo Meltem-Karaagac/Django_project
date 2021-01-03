3ef276df7e2cbdcc888b762fc0386792be184b35
meltem <meltemkaraagac19@gmail.com>
2021-01-02 22:09:56 -0500

    add

--- clarusway/fscohort/models.py
+++ clarusway/fscohort/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 
 class Student(models.Model):
-    sir_name = models.CharField(max_length=50)
+    sir_name = models.CharField(max_length=60)
     last_name = models.CharField(max_length=50)
     number = models.IntegerField()
     
